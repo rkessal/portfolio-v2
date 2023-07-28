667afc3e7e85266fdf08e7e23159477ca7dc6e02
rkessal <kessal.rayhan25@gmail.com>
2023-07-28 10:52:21 +0200

    added inter font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Gloock&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;700;800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body className="text-white">
         <Main />
